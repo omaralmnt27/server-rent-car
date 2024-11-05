fc7eef068efc8e111c6fa247e6defda9e16b5f9e
Omar Almonte <omaralmnt@gmail.com>
2024-11-05 11:07:08 -0500

    arreglo

--- bd/conexion.js
+++ bd/conexion.js
@@ -1,15 +1,15 @@
 const { Pool } = require("pg");
-require("dotenv").config()
+//require("dotenv").config()
 
 
-const devConfig = {
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+//const devConfig = {
+//  user: process.env.PG_USER,
+ // host: process.env.PG_HOST,
+  //database: process.env.PG_DATABASE,
+  //password: process.env.PG_PASSWORD,
+  //port: process.env.PG_PORT,
 
-}
+//}
 
 const proConfig = {
 
