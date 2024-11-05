a38c5592bab3040790782159e9a1bda0a4442f91
Omar Almonte <omaralmnt@gmail.com>
2024-11-05 10:08:22 -0500

    push inicial

--- bd/conexion.js
+++ bd/conexion.js
@@ -1,11 +1,22 @@
 const { Pool } = require("pg");
+require("dotenv").config()
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'rentcar',
-  password: '12345',
-  port: 5432,
-});
+
+const devConfig = {
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+
+}
+
+const proConfig = {
+
+  connectionString: process.env.DATABASE_URL
+
+
+}
+const pool = new Pool(proConfig);
 
 module.exports = pool; 
