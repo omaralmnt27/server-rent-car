213671a766ecc92aabc4eadaa41a26d5e5376d3d
Omar Almonte <omaralmnt@gmail.com>
2024-11-07 17:45:59 -0500

    documento

--- rutas/r_documento.js
+++ rutas/r_documento.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getTipoDocumento } = require('../bd/tablas/documento');
 
-router.get('/tipotelefono', getTipoDocumento);
+router.get('/tipodocumento', getTipoDocumento);
 
 module.exports = router;
