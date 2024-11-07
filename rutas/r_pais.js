e30786890b345b2932705f67187dc0206373156d
Crisbel Peralta <crisbel25c@gmail.com>
2024-11-07 19:36:28 -0400

    cambios

--- rutas/r_pais.js
+++ rutas/r_pais.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getPais } = require('../bd/tablas/pais');
 
-router.get('/pais', getPais);
+router.get('/', getPais);
 
 module.exports = router;
