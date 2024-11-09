290b0300dfd0cad6b66712263fdf7d050860d9b8
Crisbel Peralta <crisbel25c@gmail.com>
2024-11-08 22:57:52 -0400

    Avances en direcciones

--- rutas/r_estado.js
+++ rutas/r_estado.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getEstado } = require('../bd/tablas/estado');
 
-router.get('/', getEstado);
+router.get('/1', getEstado);
 
 module.exports = router;
