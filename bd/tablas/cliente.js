99ab275cf9789d572c4972cc5f737c0cad1cb42f
Crisbel Peralta <crisbel25c@gmail.com>
2024-11-10 19:58:51 -0400

    Incorporar pais

--- bd/tablas/cliente.js
+++ bd/tablas/cliente.js
@@ -159,7 +159,7 @@ SELECT
     p.nombre,
     p.apellido,
     p.fecha_nacimiento,
-    pa.descripcion AS pais_entidad,
+    pa.descripcion AS pais,
     
     -- Concatenar todos los teléfonos junto con su tipo en una sola cadena
     COALESCE(
