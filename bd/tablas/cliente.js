c53269a1afa5fac69bb23f37d1fd23c201962ba9
Omar Almonte <omaralmnt@gmail.com>
2024-11-09 19:27:51 -0500

    Correciones documentos y telefonos

--- bd/tablas/cliente.js
+++ bd/tablas/cliente.js
@@ -43,7 +43,7 @@ const insertTelefonos = async (entidadId, telefonos) => {
 
 // Función para insertar documentos
 const insertDocumentos = async (entidadId, documentos) => {
-    const query = 'INSERT INTO documento (id_entidad, id_tipo_documento, numero, fecha_emision, fecha_vencimiento, pais) VALUES ($1, $2, $3, $4, $5, $6)';
+    const query = 'INSERT INTO documento (id_entidad, id_tipo_documento, numeracion, fecha_emision, fecha_vencimiento, pais) VALUES ($1, $2, $3, $4, $5, $6)';
     try {
         for (const doc of documentos) {
             await pool.query(query, [entidadId, doc.tipo_documento, doc.numero, doc.fecha_emision, doc.fecha_vencimiento, doc.pais]);
