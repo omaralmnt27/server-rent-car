e9e48c1a7ff3511dbbcb5f76e2a4a29739a17d01
Crisbel Peralta <crisbel25c@gmail.com>
2024-11-11 23:31:21 -0400

    Creacion de ruta para tabla de combustibles

--- bd/tablas/traccion.js
+++ bd/tablas/traccion.js
@@ -2,7 +2,7 @@ const pool = require('../conexion');
 
 const getTraccion = async (req, res) => {
   try {
-    const result = await pool.query('SELECT id_traccion AS id, descripcion FROM tipo_traccion');
+    const result = await pool.query('SELECT id_tipo_traccion AS id, descripcion FROM tipo_traccion');
     res.status(200).json(result.rows); // Enviar el resultado como JSON
   } catch (error) {
     console.error("Error al obtener tracciones:", error);
