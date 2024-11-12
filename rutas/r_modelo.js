e2291ff33bca0d248beb624eb8952698aa160b82
Crisbel Peralta <crisbel25c@gmail.com>
2024-11-12 11:46:57 -0400

    Rutas de modelo

--- rutas/r_modelo.js
+++ rutas/r_modelo.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getModelo } = require('../bd/tablas/modelo');
 
-router.get('/', getModelo);
+router.post('/', getModelo);
 
 module.exports = router;
