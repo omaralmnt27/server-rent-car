469acc7bd8fe94629cd6cb0522ce150b5f269126
Crisbel Peralta <crisbel25c@gmail.com>
2024-11-12 21:09:34 -0400

    Lista de vehiculos

--- bd/tablas/vehiculo.js
+++ bd/tablas/vehiculo.js
@@ -65,13 +65,17 @@ const getVehiculo = async () => {
                 v.cantidad_puerta, 
                 v.cantidad_pasajero, 
                 c.descripcion AS color,
-                ve.descripcion AS version, 
+                ve.descripcion AS version,
+                mo.descripcion AS modelo, 
+                m.descripcion AS marca,
                 tv.descripcion AS tipo_vehiculo,
                 ev.descripcion AS estado_vehiculo, 
                 tt.descripcion AS tipo_traccion
             FROM vehiculo v
             INNER JOIN color c ON v.id_color = c.id_color
-            INNER JOIN version ve ON v.id_version = ve.id_version
+            INNER JOIN version ve ON v.id_version = ve.id_version,
+            INNER JOIN modelo mo ON mo.id_modelo = ve.id_modelo,
+            INNER JOIN marca m ON mo.id_marca = m.id_marca,
             INNER JOIN tipo_vehiculo tv ON v.id_tipo_vehiculo = tv.id_tipo_vehiculo
             INNER JOIN estado_vehiculo ev ON v.id_estado_vehiculo = ev.id_estado_vehiculo
             INNER JOIN tipo_traccion tt ON v.id_tipo_traccion = tt.id_tipo_traccion
